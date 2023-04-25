08942ba550cb346feb1b6116755fc115eed84aa8
dongho-ham <dong970827@naver.com>
2023-04-25 11:55:27 +0900

    Fix: VGG_architecture

--- 4주차/homework_practice/networks/VGG.py
+++ 4주차/homework_practice/networks/VGG.py
@@ -23,13 +23,20 @@ class VGG_block(nn.Module):
         ])
         kernel_size=1 if last_1conv else 3
         self.last_convs = VGG_conv(out_channels, out_channels, kernel_size=kernel_size)
-        self.maxpool = nn.MaxPool2d()
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+    
+    def forward(self, x):
+        x = self.first_conv(x)
+        for module in self.min_conv:
+            x = module(x)
+        x = self.last_convs(x)
+        return x
 
 class VGG_classifier(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.FC = nn.Sequential(
-            nn.Linear(32786, 4096),
+            nn.Linear(25088, 4096),
             nn.Linear(4096, 4096),
             nn.Linear(4096, num_classes))
         
